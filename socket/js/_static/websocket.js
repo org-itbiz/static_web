4734a66a922feef616831b77570d996a2a0f92a6
jung <prodigi1@hotmail.com>
2018-07-24 14:28:53 +0900

    socket demo 수정

--- socket/js/_static/websocket.js
+++ socket/js/_static/websocket.js
@@ -1,33 +1,34 @@
 function onOpen(evt) {
-    recvMemo.text(recvMemo.text() + 'connected \r\n').html();
+    // recvMemo.text(recvMemo.text() + 'connected \r\n').html();
     // curr_ws = clearInterval(curr_ws);
 }
 
 function onClose(evt) {
-    recvMemo.text(recvMemo.text() + 'close \r\n').html();
+    // recvMemo.text(recvMemo.text() + 'close \r\n').html();
 }
 
 function onMessage(evt) {
     var message = evt.data;
 
-    recvMemo.text(recvMemo.text() + message + ' \r\n').html();
+    // recvMemo.text(recvMemo.text() + message + ' \r\n').html();
 }
 
 function onError(evt) {
     console.log(evt);
     var message = evt.data;
 
-    recvMemo.text(recvMemo.text() + message + ' \r\n').html();
+    // recvMemo.text(recvMemo.text() + message + ' \r\n').html();
 }
 
 function sendMessage(text) {
     if (websocket == undefined) {
         console.log(text);
+        alert("websocket undefined.");
     } else {
         websocket.send(text);
     }
 
-    recvMemo.text(recvMemo.text() + text + '\r\n').html();
+    // recvMemo.text(recvMemo.text() + text + '\r\n').html();
 }
 
 function loadWebsocket(wsPort) {
@@ -54,7 +55,7 @@ function currentWebsocket(wsPort) {
 }
 
 function show_console(arg) {
-    recvMemo.text(recvMemo.text() + arg + '\r\n').html();
+    // recvMemo.text(recvMemo.text() + arg + '\r\n').html();
 }
 
 // 전역변수
@@ -63,7 +64,7 @@ var websocket, recvMemo, curr_ws;
 $(function () {
     recvMemo = $('#ws_debug');
 
-    // currentWebsocket(33333);
+    currentWebsocket(30001);
     // curr_ws = setInterval(function () {
     //     currentWebsocket(33333);
     // }, 500);
