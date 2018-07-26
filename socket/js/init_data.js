f1161f26c10fec9e5c8ecdb6f121450795cf1d36
jung <prodigi1@hotmail.com>
2018-07-26 09:28:42 +0900

    updated

---
+++ socket/js/init_data.js
@@ -0,0 +1,16 @@
+var urls = {
+    'web1':'http://iotc365.com/test_locale/web1/web1.json',
+
+};
+
+var init_sel_value = [       
+    { "value": "1", "text": "value1"  },
+    { "value": "2", "text": "value2"  },
+    { "value": "3", "text": "value3"  }
+];
+
+var init_chk_value = [
+    {"value": "1", 'text':'사용'}
+];
+
+var web1 = {};
